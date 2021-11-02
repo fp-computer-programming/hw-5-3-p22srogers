864ed5b8661b140c419affa6c34c938574f7e4cf
P22srogers <p22srogers@fairfieldprep.org>
2021-11-02 12:35:08 -0400

    Submission

---
+++ hw5-3.py
@@ -0,0 +1,15 @@
+# Author: SMR (AMDG) 11/02/21
+import calendar
+# Question 1:
+calendar.TextCalendar().pryear(2020)
+# Question 2
+calendar.setfirstweekday(calendar.SUNDAY)
+print(calendar.prcal(2020))
+# Question 3
+calendar.prmonth(2003,9)
+# Question 4
+# calendar.LocaleTextCalendar(6, "SPAINISH").pryear(2020)
+# Question 5
+print(calendar.isleap(2003))
+# This command determines whether or not the given year is a leap year. The outcome of this statement will returns either True or False, making it is a boolean value.
+
